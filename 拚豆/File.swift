5cd40c7bd8473eecfa916a27143d616aa27da957
Chiu <jacky10045@icloud.com>
2019-06-28 11:21:08 +0800

    Initial Commit

---
+++ 拚豆/File.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  拚豆
+//
+//  Created by ios on 2019/5/10.
+//  Copyright © 2019 Zonghan. All rights reserved.
+//
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
